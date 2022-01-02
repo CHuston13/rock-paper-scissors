780a9cec4268b99de9f256a41fd6dcebebfb75b6
Chuston13 <Cdhprogrammer@gmail.com>
2022-01-02 06:23:22 -0600

    index.html

---
+++ js.js
@@ -0,0 +1,18 @@
+let computerScore=0;
+let playerScore=0;
+
+
+
+function computerPlay() {
+    let choices = ['rock', 'paper', 'scissors']
+    return choices[Math.floor(Math.random() * choices.length)]
+}
+
+function game(){
+   let PlayerChoice = ['Rock','Paper','Scissors']
+    if rock='Rock'
+    return choices[Math.Floor(Math.random() * choice.length];
+    
+}
+
+
