4fc2d5d09711382928d57553f306c49fdf671ad9
Chuston13 <Cdhprogrammer@gmail.com>
2022-01-02 10:36:09 -0600

    js.js

--- js.js
+++ js.js
@@ -1,18 +1,24 @@
 let computerScore=0;
 let playerScore=0;
 
-
+function playerSelection(){
+    let playerSelection=prompt('Rock','Paper','Scissors')}
+    
+}
 
 function computerPlay() {
     let choices = ['rock', 'paper', 'scissors']
     return choices[Math.floor(Math.random() * choices.length)]
 }
+ 
 
-function game(){
-   let PlayerChoice = ['Rock','Paper','Scissors']
-    if rock='Rock'
-    return choices[Math.Floor(Math.random() * choice.length];
-    
-}
+
+function PlayRound(computerPlay, playerSelection)
+if (playerSelection==='rock' && computerPlay==='Paper')
+return 'Paper beats rock';
+else (playerSelection=== 'paper' && computerPlay==='scissors')
+return 'Scissors beat rock'
+
+
 
 
