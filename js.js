5d2b053cb16d0d50a3f782a0b7112056a9acebeb
Chuston13 <Cdhprogrammer@gmail.com>
2022-01-02 15:02:24 -0600

    index.html

--- js.js
+++ js.js
@@ -1,24 +1,57 @@
-let computerScore=0;
-let playerScore=0;
 
-function playerSelection(){
-    let playerSelection=prompt('Rock','Paper','Scissors')}
-    
-}
+let playerScore = 0
+let computerScore = 0
+const buttons = document.querySelectorAll('input')
 
 function computerPlay() {
     let choices = ['rock', 'paper', 'scissors']
     return choices[Math.floor(Math.random() * choices.length)]
 }
- 
 
+function disableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = true
+    })
+}
 
-function PlayRound(computerPlay, playerSelection)
-if (playerSelection==='rock' && computerPlay==='Paper')
-return 'Paper beats rock';
-else (playerSelection=== 'paper' && computerPlay==='scissors')
-return 'Scissors beat rock'
+function playRound(playerSelection) {
+    let computerSelection = computerPlay()
+    let result =''
 
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'rock')) {
+        
+        playerScore += 1
+        result = ('You win! ' + playerSelection + ' beats ' + computerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
 
+        if (playerScore == 5) {
+            result += '<br><br>You won the game! Reload the page to play again'
+            disableButtons()
+        }
+    }
+    else if (playerSelection == computerSelection) {
+        result = ('It\'s a tie. You both chose ' + playerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
+    }
+    else {
+        computerScore += 1
+        result = ('You lose! ' + computerSelection + ' beats ' + playerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
 
+        if (computerScore == 5) {
+            result += '<br><br>I won the game! Reload the page to play again'
+            disableButtons()
+        }
+    }
 
+    document.getElementById('result').innerHTML = result
+    return
+}
+
+buttons.forEach(button =>{
+    button.addEventListener('click', function(){
+        playRound(button.value)
+    })
+})
